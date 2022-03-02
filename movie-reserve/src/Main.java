63939c6a578df808316460e0149dcbe1cca61cea
moduhan <devduhanmo@gmail.com>
2022-03-02 23:58:31 +0900

    ch02. 영화예매 애플리케이션 수정, doc: readme 내용정리

--- movie-reserve/src/Main.java
+++ movie-reserve/src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.event.ModificationWatchpointEvent;
+
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
@@ -22,5 +24,15 @@ public class Main {
                         new SequenceCondition(2),
                         new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59)))
         );
+
+        Movie starWars = new Movie("스타워즈",
+                Duration.ofMinutes(210),
+                Money.wons(10000),
+                new NoneDiscountPolicy());
+
+        avatar.changeDiscountPolicy(new PercentDiscountPolicy(0.1,
+                new PeriodCondition(DayOfWeek.TUESDAY, LocalTime.of(14, 0), LocalTime.of(16, 59)),
+                new SequenceCondition(2),
+                new PeriodCondition(DayOfWeek.THURSDAY, LocalTime.of(10, 0), LocalTime.of(13, 59))));
     }
 }
